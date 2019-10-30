0fe9559b03fc6ff1d63ef840de80dac6f49a3b87
Raul Juliao <raulj@uninorte.edu.co>
2019-10-29 22:47:06 -0500

    Cambios menores

--- src/views/Menu/components/index.js
+++ src/views/Menu/components/index.js
@@ -2,3 +2,4 @@ export { default as InformationCard } from './InformationCard';
 export { default as Goals } from './Goals';
 export { default as Dashboard } from './Dashboard';
 export { default as Paraclinicos } from './Paraclinicos';
+export { default as Messages } from './Messages';
