ef7208d449b86ef1b44dc68c406d9f2a63e8d3bd
Raul Juliao <raulj@uninorte.edu.co>
2019-11-07 20:02:45 -0500

    Borrar una meta

--- src/views/Menu/components/Goals/components/TableContent/TableContent.js
+++ src/views/Menu/components/Goals/components/TableContent/TableContent.js
@@ -3,8 +3,7 @@ import clsx from 'clsx';
 import moment from 'moment';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import PropTypes from 'prop-types';
-
-import MaterialTable from 'material-table';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import { makeStyles } from '@material-ui/styles';
 import { Card, 
     CardContent, 
@@ -14,7 +13,9 @@ import { Card,
     TableRow, 
     TableBody,  
     CardActions,
-    TablePagination} from '@material-ui/core';
+    TablePagination,
+    IconButton} from '@material-ui/core';
+import { deleteGoal } from '../../../../../../services/api';
 
 
 const useStyles = makeStyles(theme => ({
@@ -42,7 +43,7 @@ const useStyles = makeStyles(theme => ({
 
 
 const TableContent = props => {
-    const { className, goals, ...rest } = props;
+    const { className,history, goals, ...rest } = props;
   
 
     const classes = useStyles();
@@ -51,12 +52,25 @@ const TableContent = props => {
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const [page, setPage] = useState(0);
     const handlePageChange = (event, page) => {
-        setPage(page);
-      };
-    
-      const handleRowsPerPageChange = event => {
-        setRowsPerPage(event.target.value);
-      };
+      setPage(page);
+    };
+  
+    const handleRowsPerPageChange = event => {
+      setRowsPerPage(event.target.value);
+    };
+
+    const handleDelete = id =>{
+      deleteGoal(id)
+      .then(response => {
+        return response.json();
+      })  
+      .then(json => {
+        history.push('./menu')
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+    }
     
     return (
         <Card
@@ -76,35 +90,46 @@ const TableContent = props => {
                         <TableCell>Cantidad</TableCell>
                         <TableCell>Frecuencia</TableCell>
                         <TableCell>Progreso de la meta</TableCell>
+                        <TableCell>Eliminar</TableCell>
                         </TableRow>
                     </TableHead>
                     
                     <TableBody>
                         {goals.slice(0, rowsPerPage).map(goal => (
                         <TableRow
-                            className={classes.tableRow}
-                            hover
-                            key={goal.id}
-                            selected={selectedUsers.indexOf(goal.id) !== -1}
+                          className={classes.tableRow}
+                          hover
+                          key={goal.id}
+                          selected={selectedUsers.indexOf(goal.id) !== -1}
                         >
-                            
-                            <TableCell>              
-                                {moment(goal.cretationDate).format('DD/MM/YYYY')}
-                            </TableCell>
-                            
-                            <TableCell>              
-                                {moment(goal.dueDate).format('DD/MM/YYYY')}
-                            </TableCell>
-                            <TableCell component="th" scope="row">{goal.description}</TableCell>
-                            <TableCell>{goal.state==='2'?"Asigando":"Cumplida"} 
-                            </TableCell>
-                            <TableCell>{goal.quantity} </TableCell>
-                            <TableCell>
-                              {goal.frequency==='1'? "Cada Hora":
-                              (goal.frequency==='2'?"Diariamente":
-                              (goal.frequency==='3'?"Semanalmente":"Mensualmente"))} 
-                            </TableCell>
-                            <TableCell>{(goal.progress/goal.quantity)*100}%</TableCell>
+                          <TableCell>              
+                            {moment(goal.cretationDate).format('DD/MM/YYYY')}
+                          </TableCell>
+                          <TableCell>              
+                            {moment(goal.dueDate).format('DD/MM/YYYY')}
+                          </TableCell>
+                          <TableCell component="th" scope="row">
+                            {goal.description}
+                          </TableCell>
+                          <TableCell>
+                            {goal.state==='2'?"Asigando":"Cumplida"} 
+                          </TableCell>
+                          <TableCell>
+                            {goal.quantity}
+                          </TableCell>
+                          <TableCell>
+                            {goal.frequency==='1'? "Cada Hora":
+                            (goal.frequency==='2'?"Diariamente":
+                            (goal.frequency==='3'?"Semanalmente":"Mensualmente"))} 
+                          </TableCell>
+                          <TableCell>
+                            {(goal.progress/goal.quantity)*100}%
+                          </TableCell>
+                          <TableCell>
+                            <IconButton aria-label="delete" onClick={()=>handleDelete(goal._id)}>
+                              <DeleteForeverIcon fontSize="large" />
+                            </IconButton>
+                          </TableCell>
                         </TableRow>
                         ))}
                     </TableBody>
